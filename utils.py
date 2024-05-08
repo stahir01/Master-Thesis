d4834f37beb78cc81f78cc47d6de8994ccddd0f4
Ali Murad <alimurad00@hotmail.com>
2024-05-08 18:02:12 +0200

    All previous data cleanup done

--- utils.py
+++ utils.py
@@ -52,6 +52,27 @@ def convert_length_to_meters(dataset, *parameters, ft=False, cm=False):
     
     return dataset
 
+def convert_speed(dataset, *parameters, knots=False):
+    """
+    Convert speed from knots to kilometers per hour (km/h).
+    
+    Args:
+        dataset: pd.DataFrame
+        parameters: list of columns to convert
+        knots_to_kmh: True if the speed is in knots and needs to be converted to km/h, False otherwise
+        
+    Returns:
+        dataset: pd.DataFrame
+    """
+    for param in parameters:
+        dataset[param] = dataset[param].apply(pd.to_numeric, errors='coerce')
+        
+        # Convert knots to km/h
+        if knots:
+            dataset[param] = dataset[param].apply(lambda x: x * 1.852)
+    
+    return dataset
+
 
 
 def convert_loading(dataset, *parameters, kg_m2=False, lbs_ft2=False):
