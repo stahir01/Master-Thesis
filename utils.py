e92d61cf74e7433ffa8b8fccf89095c5ebcd9bf8
Ali Murad <alimurad00@hotmail.com>
2024-07-17 18:37:39 +0200

    Added methods of visualization feature importance for the CNN model

--- utils.py
+++ utils.py
@@ -5,9 +5,15 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
-from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, f1_score, precision_score, mean_absolute_error, mean_squared_error, r2_score, ConfusionMatrixDisplay
+from sklearn.metrics import (
+    confusion_matrix, classification_report, accuracy_score, f1_score, precision_score,
+    mean_absolute_error, mean_squared_error, r2_score, ConfusionMatrixDisplay
+)
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import shap
+from IPython.display import display
+
+
 
 
 #from googlesearch import search
@@ -500,6 +506,75 @@ def plt_feature_importance(model, feature_names):
     plt.xlabel('Importance Score')
     plt.ylabel('Feature')
     plt.show()    
+
+
+class CNNFeatureImportance:
+    def __init__(self, model, X_train, X_test, features, index=0):
+        """
+        Initialize the CNNFeatureImportance class.
+
+        Args:
+            model: The trained CNN model.
+            X_train: The training data.
+            X_test: The testing data.
+            features: The feature names.
+            index: The index of the instance to explain. Default is 0.
+        """
+        self.model = model
+        self.X_train = X_train
+        self.X_test = X_test
+        self.features = features
+        self.index = index
+
+        # Reshape the data to match the input shape of the CNN model
+        self.X_train_reshaped = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
+        self.X_test_reshaped = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+
+        # Initialize the JS visualization code for Jupyter Notebook
+        shap.initjs()
+
+        # Create a SHAP DeepExplainer for the CNN model
+        self.explainer = shap.DeepExplainer(self.model, self.X_train_reshaped)
+
+        # Calculate SHAP values
+        self.shap_values = self.explainer.shap_values(self.X_test_reshaped)
+
+        # Reshape the SHAP values for the force plot
+        self.shap_values_reshaped = self.shap_values[self.index].reshape(-1, X_test.shape[1])
+        self.instance_shap_values = self.shap_values[self.index].reshape(-1)
+
+        # Convert tensor value to float
+        self.expected_value = self.explainer.expected_value[0].numpy() if hasattr(self.explainer.expected_value[0], 'numpy') else float(self.explainer.expected_value[0])
+
+    def plot_summary(self):
+        """
+        Plot the summary plot for feature importance.
+        """
+
+        shap.summary_plot(self.shap_values_reshaped, self.X_test, feature_names=self.features, plot_type="bar", color='maroon')
+
+    def plot_force(self):
+        """
+        Plot the force plot for a specific instance.
+        """
+
+        force_plot = shap.force_plot(base_value=self.expected_value, shap_values=self.shap_values_reshaped, features=self.X_test[self.index], feature_names=self.features)
+        display(force_plot)
+    
+    def plot_waterfall(self):
+        """
+        Plot the waterfall plot for a specific instance.
+
+        Args:
+            None
+        
+        Returns:
+            None
+        """
+        shap.waterfall_plot(
+        shap.Explanation(values=self.instance_shap_values,  base_values=self.expected_value,  data=self.X_test[self.index], feature_names=self.features))
+
+        
 
 
 if __name__ == '__main__':
